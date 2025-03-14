4442d26f0da664b7e30ca4fb853a1e6b976006cc
Monika-rao <monikarao1710@gmail.com>
2025-03-14 13:15:45 +0530

    java 8 Assignments

---
+++ src/main/java/org/example/Java8/Employee.java
@@ -0,0 +1,50 @@
+package org.example.Java8;
+
+public class Employee {
+
+    int age;
+    String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Employee(int age, String name, double salary) {
+        this.age = age;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    double salary;
+
+
+}
